938960a3c068eae9e4f6fc7ffc759004e5f147ea
JimmyAllDay <jameswhmarshall@gmail.com>
2021-05-15 08:40:21 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Webpack Boiler Plate
+
+Node, Express.js, Sass, various webpack loaders and plugins.
+
+Shut up, it runs.
+
+Instructions:
+
+1. Clone the repository
+
+2. Run npm install in the project directory
+
+3. For development, run 'npm run nodemon' and, in another terminal window 'npm run build-dev'.
+   This will start a development environment with hot reloading, etc.
+
+4. For prod, run 'npm run build-prod', then 'npm start'
+   This will build a production version of the app and serve it via Node. The Node server listens on port 8080.
---
+++ webpack.dev.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.js',
+  mode: 'development',
+  devtool: 'source-map',
+  stats: 'verbose',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: '/.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html'
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
+    })
+  ]
+};
